7fbc06f5c8f36257e1906ee5e06864ee28fb12ea
german-jongewaard <germanjongewaard@gmail.com>
2019-05-13 15:23:31 +0200

    Hotel Management System.

---
+++ src/java_hotel_system/MY_CONNECTION.java
@@ -0,0 +1,47 @@
+package java_hotel_system;
+
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author german
+ */
+public class MY_CONNECTION {
+    // In this class we will make our connection with the mysql database
+    // 1.- download the mysql connector
+    // 2.- extract the file
+    // 3.- add the connector to your project
+    // 4.- open xampp
+    // 5.- strt apche and mysql
+    // 6.- go to phpmyadmin
+    // 7.- create the database
+    
+    //create a funcion to connect with mysql
+    
+    public Connection createConnection()
+    {
+        Connection connection = null;
+        
+        MysqlDataSource mds = new MysqlDataSource();
+        
+        mds.setServerName("localhost");
+        mds.setPortNumber(3306);
+        mds.setUser("root");
+        mds.setPassword("");
+        mds.setDatabaseName("java_hotel_db");
+        
+        try {
+            connection = mds.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(MY_CONNECTION.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+        
+        return connection;        
+    }    
+}
