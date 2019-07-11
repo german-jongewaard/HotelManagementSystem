10cc8e76a0a2d4888b53630dbc898a220d4145e8
german-jongewaard <germanjongewaard@gmail.com>
2019-07-11 14:26:37 +0200

    Hotel Management System.

---
+++ src/java_hotel_system/CLIENT.java
@@ -0,0 +1,27 @@
+package java_hotel_system;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author german
+ */
+public class CLIENT { // the client class
+    
+    // create a function to add a client
+    public boolean addclient(String fname, String lname, String phone, String email)
+    {
+        PreparedStatement st;
+        String addQuery = "";
+        
+        
+        return true;
+    }
+    
+    // create a function to edit the selected client
+    
+    // create a function to remove the selected client
+    
+    // create a function to return a lis of all clients 
+    
+}
