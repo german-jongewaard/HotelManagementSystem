1f2a4bce32912d1c08bf50b74e62dca25cf5c9fa
german-jongewaard <germanjongewaard@gmail.com>
2019-09-03 12:10:51 +0200

    Hotel Management System.

---
+++ src/java_hotel_system/RESERVATION.java
@@ -0,0 +1,191 @@
+package java_hotel_system;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author german
+ */
+public class RESERVATION {
+    
+    // in the reservation table we need to add two FOREIG N keys:
+    // 1 for the client
+    // --> alter TABLE reservations ADD CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES clients (id) on DELETE CASCADE    
+    // 2 for the room
+    // --> alter TABLE reservations ADD CONSTRAINT fk_room_number FOREIGN KEY (room_number) REFERENCES rooms (r_number) on DELETE CASCADE
+    // and add another foreign key between table types and rooms
+    //--> alter TABLE rooms ADD CONSTRAINT fk_type_id FOREIGN KEY (type) REFERENCES type(id) on DELETE CASCADE
+    
+    // Some fixes we need to do
+    // 1 - When we ad a new reservation the room associated with it should be set to reserved = YES
+    //     and when deleting the reservation it should be set to reserved = NO
+    // 2 - When we add a new reservation we need to check if the room is already reserved
+    // 3 - check if the date in > the curren date
+    // 4 - check if the date out > the date in
+    
+    
+    MY_CONNECTION my_connection = new MY_CONNECTION();
+    
+    ROOMS room = new ROOMS();
+    
+    
+     
+    
+    // create a function to add a new room
+    public boolean addReservation(int client_id, int room_number, String dateIn, String dateOut)
+    {
+        PreparedStatement st;
+        String addQuery = "INSERT INTO `reservations`(`client_id`, `room_number`, `date_in`, `date_out`) VALUES (?,?,?,?)";
+        
+        try {            
+            st = my_connection.createConnection().prepareStatement(addQuery);
+            
+            st.setInt(1, client_id);
+            st.setInt(2, room_number);
+            st.setString(3, dateIn);             
+            st.setString(4, dateOut); 
+            
+            if (room.isRoomToReserved(room_number).equals("No")) {
+                
+                 if (st.executeUpdate() > 0){                
+                        room.setRoomToReservation(room_number, "Yes");
+                        return true;
+                }else{
+                        return false;
+                     }
+            }else{
+                JOptionPane.showMessageDialog(null,"This Room Is Already Reserved", "Room Reserved", JOptionPane.WARNING_MESSAGE);            
+                return false;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+     
+     // create a function to edit the selected room
+    public boolean editReservation(int reservation_id, int client_id, int room_number, String  dateIn, String dateOut)
+    {
+        PreparedStatement st;
+        String editQuery = "UPDATE `reservations` SET `client_id`=?,`room_number`=?,`date_in`=?,`date_out`=? WHERE `id`=?";
+        
+        try {            
+            st = my_connection.createConnection().prepareStatement(editQuery);            
+            
+            st.setInt(1, client_id);
+            st.setInt(2, room_number);
+            st.setString(3, dateIn); 
+            st.setString(4, dateOut); 
+            st.setInt(5, reservation_id);
+            
+            return (st.executeUpdate() > 0);
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    // create a function to remove the selected reservation
+     public boolean removeReservation(int reservation_id)
+    {
+        PreparedStatement st;
+        String deleteQuery = "DELETE FROM `reservations` WHERE `id`=?";
+        
+        try {            
+            st = my_connection.createConnection().prepareStatement(deleteQuery);            
+           
+            st.setInt(1, reservation_id); 
+            
+            int room_number = getRoomNumberfromreservation(reservation_id);
+            
+            if (st.executeUpdate() > 0){
+
+                //room.setRoomToReservation(reservation_id, "No");
+                room.setRoomToReservation(room_number, "No");
+
+                return true;
+            }else{
+                
+                return false;
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+     
+// create a function to display all rooms in jtable
+    public void fillReservationsTable(JTable table){
+        
+        PreparedStatement ps;
+        ResultSet rs;
+        String selectQuery = "SELECT * FROM `reservations`";
+
+        try {
+            
+            ps = my_connection.createConnection().prepareStatement(selectQuery);
+            
+            rs = ps.executeQuery();
+            
+            DefaultTableModel  tableModel = (DefaultTableModel) table.getModel();
+            
+            Object[] row;
+            
+            while(rs.next())
+            {
+                row = new Object[5];
+                row[0] = rs.getInt(1);
+                row[1] = rs.getInt(2);
+                row[2] = rs.getInt(3);
+                row[3] = rs.getString(4);
+                row[4] = rs.getString(5);
+                
+                tableModel.addRow(row);                
+            }                      
+        } catch (SQLException ex) {
+            Logger.getLogger(CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }  
+    
+    // Create a function to get the room  number from a reservation 
+    public int getRoomNumberfromreservation(int reservation_id)
+    {    
+        PreparedStatement ps;
+        ResultSet rs;
+        String selectQuery = "SELECT `room_number` FROM `reservations` WHERE `id`=?";
+        
+         try {
+            
+            ps = my_connection.createConnection().prepareStatement(selectQuery);
+            
+            ps.setInt(1, reservation_id);
+            
+            rs = ps.executeQuery();            
+            
+            
+             if (rs.next()) {
+                 
+                 return rs.getInt(1);
+             }else{
+                 return 0;
+             }
+                                    
+        } catch (SQLException ex) {
+            Logger.getLogger(CLIENT.class.getName()).log(Level.SEVERE, null, ex);
+            return 0;
+        } 
+         
+    }
+    
+}
